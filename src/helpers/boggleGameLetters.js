5dfa5da0d6a58102a9df4c13165e051d64a93bca
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-14 09:26:44 -0400

    boggleGameLetters function for generating random game board letters

---
+++ src/helpers/boggleGameLetters.js
@@ -0,0 +1,10 @@
+import { alphabet } from '../helpers/alphabet';
+
+export function boggleGameLetters() {
+  let boggleGameLetters = [];
+  for (let i = 0; i < 16; i++) {
+    boggleGameLetters.push(alphabet[Math.floor(Math.random() * 25 + 1)]);
+  }
+
+  return boggleGameLetters;
+}
